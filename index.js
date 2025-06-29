dd88ca63b25047f36b14b02e6bd4cf16b68d9e66
ayush00git <ayush.2007.iit@gmail.com>
2025-06-29 18:55:30 +0530

    Server setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// ### Let's get Started ### 
+const express = require("express")
+
+const app = express()
+const PORT = process.env.PORT || 8001
+
+
+app.get('/', (req, res) => {
+    return res.send("Hello from the nodejs server")
+})
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`))
