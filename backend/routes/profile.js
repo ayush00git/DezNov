91504daf54812618784b74a8fddca4535d82026b
ayush00git <ayush.2007.iit@gmail.com>
2025-08-12 22:47:48 +0530

    profile-page-backend

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -5,12 +5,35 @@ const route = express.Router();
 const User = require('../models/user');
 
 
-// GET request for profileSteup
+// GET request for profileSetup
 route.get('/profileSetup', (req, res) => {
     try {
         return res.status(200).json({ message: 'Setting up a profile (page loaded)' });
     } catch (error) {
         return res.status(400).json({ message: `Error: ${error}` });
+    }
+})
+
+// POST request for profileSetup
+route.post('/profileSetup', async(req, res) => {
+    try {
+
+        const { userName, fullName, email } = req.user;
+        const { title, aboutText, github, linkedin, portfolio } = req.body;
+
+        await Profile.create({
+            userName,
+            fullName,
+            email,
+            title, 
+            aboutText,
+            github,
+            linkedin,
+            portfolio,
+        })
+        return res.status(200).json({ message: 'Profile information get in' });
+    } catch (error) {
+        return res.status(500).json({ message: `Error: ${error}` })
     }
 })
 
